c731563eceda30ad0db317d95b40fce0c94b7c70
Jeremy Jones <banjeremy@gmail.com>
2014-10-19 13:53:42 -0500

    initial commit. lots of congif

---
+++ Gruntfile.js
@@ -0,0 +1,196 @@
+/*jslint node: true */
+'use strict';
+
+var pkg = require('./package.json');
+
+//Using exclusion patterns slows down Grunt significantly
+//instead of creating a set of patterns like '**/*.js' and '!**/node_modules/**'
+//this method is used to create a set of inclusive patterns for all subdirectories
+//skipping node_modules, bower_components, public, and any .dirs
+//This enables users to create any directory structure they desire.
+var createFolderGlobs = function(fileTypePatterns) {
+  fileTypePatterns = Array.isArray(fileTypePatterns) ? fileTypePatterns : [fileTypePatterns];
+  var ignore = ['node_modules','bower_components','public','temp'];
+  var fs = require('fs');
+  return fs.readdirSync(process.cwd())
+          .map(function(file){
+            if (ignore.indexOf(file) !== -1 ||
+                file.indexOf('.') === 0 ||
+                !fs.lstatSync(file).isDirectory()) {
+              return null;
+            } else {
+              return fileTypePatterns.map(function(pattern) {
+                return file + '/**/' + pattern;
+              });
+            }
+          })
+          .filter(function(patterns){
+            return patterns;
+          })
+          .concat(fileTypePatterns);
+};
+
+module.exports = function (grunt) {
+
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      main: {
+        options: {
+          port: 9001
+        }
+      }
+    },
+    watch: {
+      main: {
+        options: {
+            livereload: true,
+            livereloadOnError: false,
+            spawn: false
+        },
+        files: [createFolderGlobs(['*.js','*.less','*.jade']),'!_SpecRunner.html','!.grunt'],
+        tasks: [] //all the tasks are run dynamically during the watch event handler
+      }
+    },
+    jshint: {
+      main: {
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        src: createFolderGlobs('*.js')
+      }
+    },
+    clean: {
+      before:{
+        src:['public','temp']
+      },
+      after: {
+        src:['temp']
+      }
+    },
+    less: {
+      production: {
+        options: {
+        },
+        files: {
+          'temp/app.css': 'app.less'
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['img/**'], dest: 'public/'},
+          {src: ['bower_components/font-awesome/fonts/**'], dest: 'public/',filter:'isFile',expand:true}
+          //{src: ['bower_components/angular-ui-utils/ui-utils-ieshiv.min.js'], dest: 'public/'},
+          //{src: ['bower_components/select2/*.png','bower_components/select2/*.gif'], dest:'public/css/',flatten:true,expand:true},
+          //{src: ['bower_components/angular-mocks/angular-mocks.js'], dest: 'public/'}
+        ]
+      }
+    },
+    jade: {
+      build: {
+        files: [{
+          cwd: '.',
+          src: '**/*.jade',
+          dest: '.',
+          ext: '.html',
+          expand: true
+        }]
+      }
+    },
+    dom_munger:{
+      read: {
+        options: {
+          read:[
+            {selector:'script[data-concat!="false"]',attribute:'src',writeto:'appjs'},
+            {selector:'link[rel="stylesheet"][data-concat!="false"]',attribute:'href',writeto:'appcss'}
+          ]
+        },
+        src: 'public/index.html'
+      },
+      update: {
+        options: {
+          remove: ['script[data-remove!="false"]','link[data-remove!="false"]'],
+          append: [
+            {selector:'body',html:'<script src="app.full.min.js"></script>'},
+            {selector:'head',html:'<link rel="stylesheet" href="app.full.min.css">'}
+          ]
+        },
+        src:'public/index.html',
+        dest: 'public/index.html'
+      }
+    },
+    cssmin: {
+      main: {
+        src:['temp/app.css','<%= dom_munger.data.appcss %>'],
+        dest:'public/app.full.min.css'
+      }
+    },
+    concat: {
+      main: {
+        src: ['<%= dom_munger.data.appjs %>'],
+        dest: 'temp/app.full.js'
+      }
+    },
+    uglify: {
+      main: {
+        src: 'temp/app.full.js',
+        dest:'public/app.full.min.js'
+      }
+    },
+    htmlmin: {
+      main: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true
+        },
+        files: {
+          'public/index.html': 'public/index.html'
+        }
+      }
+    },
+    imagemin: {
+      main:{
+        files: [{
+          expand: true, cwd:'public/',
+          src:['**/{*.png,*.jpg}'],
+          dest: 'public/'
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('build',['jshint','clean:before','less','jade','dom_munger','cssmin','concat','uglify','copy','htmlmin','imagemin','clean:after']);
+  grunt.registerTask('serve', ['dom_munger:read','jshint','connect', 'watch']);
+
+  grunt.event.on('watch', function(action, filepath) {
+    //https://github.com/gruntjs/grunt-contrib-watch/issues/156
+
+    var tasksToRun = ['jshint'];
+
+    if (filepath.lastIndexOf('.js') !== -1 && filepath.lastIndexOf('.js') === filepath.length - 3) {
+
+      //lint the changed js file
+      grunt.config('jshint.main.src', filepath);
+      tasksToRun.push('jshint');
+    }
+
+    //if index.html changed, we need to reread the <script> tags
+    tasksToRun.push('jade');
+    if (filepath === 'index.jade') {
+      tasksToRun.push('dom_munger:read');
+    }
+
+    grunt.config('watch.main.tasks',tasksToRun);
+
+  });
+};
